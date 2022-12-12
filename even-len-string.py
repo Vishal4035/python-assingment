7c6d4320a7ee7b1bf7428444543da45c7ec76eb3
Vishal4035 <118965790+Vishal4035@users.noreply.github.com>
2022-12-12 07:07:49 -0800

    Create even-len-string.py

---
+++ even-len-string.py
@@ -0,0 +1,15 @@
+#print even lennth of a string
+def reverse(A):  
+     s = " "  
+     for i in A:  
+         s = i + s  
+     return s  
+ A=input("Enter the String to reverse")  
+ print("The original string is:",A)  
+ print("The reverse string is:", reverse(A))
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
